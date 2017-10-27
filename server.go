377004f4567cff6cde8ab9ffaa74ba0c2aef077d
Sergio Galvan <sergalma@gmail.com>
2017-10-27 23:19:13 +0200

    Provide reboot function for vps

--- server.go
+++ server.go
@@ -1,7 +1,49 @@
 package bandwagon
 
-// Server interface does basic actions for a given vps.
-type VirtualServer interface {
-	// Lists available operating systems.
-	ListImages() (*Images, error)
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+// Server provides basic operations for a given vps.
+type Server interface {
+	Reboot() (*ServerResponse, error)
+}
+
+// ServerResponse holds the Error and Message in case of failure.
+// Error:0 means no error.
+type ServerResponse struct {
+	Error   int32  `json:"error"`
+	Message string `json:"message,omitempty"`
+}
+
+// Reboot restarts the vps with a given VeID
+func (c *Client) Reboot() (*ServerResponse, error) {
+
+	veid := c.creds.VeID
+	apikey := c.creds.APIKey
+	apiPath := "v1/restart?"
+	baseURL := c.BaseURL
+
+	u := baseURL + apiPath + "veid=" + veid + "&api_key=" + apikey
+
+	ul, _ := url.Parse(u)
+	req := &http.Request{
+		URL:    ul,
+		Method: http.MethodGet,
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	response := new(ServerResponse)
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
